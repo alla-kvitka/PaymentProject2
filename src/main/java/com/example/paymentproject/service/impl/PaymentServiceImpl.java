e2da5e5774ba9ad074d1bcd49ce33216b1b27e7b
alla_kvitka <invariant17@gmail.com>
2021-11-09 06:47:48 +0200

    commit

--- src/main/java/com/example/paymentproject/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/paymentproject/service/impl/PaymentServiceImpl.java
@@ -3,4 +3,5 @@ package com.example.paymentproject.service.impl;
 import com.example.paymentproject.service.interfaces.PaymentService;
 
 public class PaymentServiceImpl implements PaymentService {
+
 }
--- src/main/java/com/example/paymentproject/service/interfaces/PaymentService.java
+++ src/main/java/com/example/paymentproject/service/interfaces/PaymentService.java
@@ -1,4 +1,5 @@
 package com.example.paymentproject.service.interfaces;
 
 public interface PaymentService {
+
 }
