b40f650b1b3b26fb35dc7e7c860a4bdc2e926860
alla_kvitka <invariant17@gmail.com>
2021-11-09 07:02:34 +0200

    commit

--- src/main/java/com/example/paymentproject/conroller/admin/AdminUnblockCardController.java
+++ src/main/java/com/example/paymentproject/conroller/admin/AdminUnblockCardController.java
@@ -14,7 +14,6 @@ public class AdminUnblockCardController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-
     }
 
     @Override
